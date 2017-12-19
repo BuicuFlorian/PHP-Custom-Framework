15107ae519e48eca442ef5fbe0ebd96ac1638a50
Florian B <floryn93@gmail.com>
2017-12-19 17:00:22 +0200

    Add partials.
    Add head, nav and footer partials.

---
+++ app/views/partials/nav.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+            <?php if (!isAuth()) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">
+                        <i class="fa fa-home"></i> Home
+                    </a>
+                </li>
+            <?php endif; ?>
+            <?php if (isAuth()) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="/tasks">
+                        <i class="fa fa-tasks"></i> Tasks
+                    </a>
+                </li>
+            <?php endif; ?>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <?php if (!isAuth()) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">
+                        <i class="fa fa-sign-in"></i> Login
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/register">
+                    <i class="fa fa-user-plus"></i> Register
+                    </a>
+                </li>
+            <?php endif; ?>
+            <?php if (isAuth()) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">
+                        <i class="fa fa-sign-out"></i> Logout
+                    </a>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </div>
+</nav>
