0b89ab1f2b2231c9b7741c19b5d13ddaa0208980
Florian B <floryn93@gmail.com>
2017-12-19 16:59:31 +0200

    Add home controller.

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController
+{   
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {   
+        if (isAuth()) {
+            return redirect('/tasks');
+        }
+    }
+
+    /**
+     * Display the view for the home page.
+     */
+    public function index()
+    {
+        $greetings = 'Simple PHP Framework';
+
+        return view('home', compact('greetings'));
+    }
+}
