6c9d4b7f674a623c13752b88724e2630775172b3
Florian B <floryn93@gmail.com>
2018-04-15 12:02:21 +0300

    Add pagination, errors and message alerts.

--- app/views/tasks.view.php
+++ app/views/tasks.view.php
@@ -1,20 +1,22 @@
 <?php require 'partials/head.php'; ?>
 
-<div class="container">
-    <h1 class="display-1">Tasks</h1>
+<?php if ($message) : ?>
+    <div class="alert alert-success alert-dismissible fade show">
+        <p class="lead"><?= $message ?></p>
+    </div>
+<?php endif; ?>
 
+<?php if ($errors) : ?>
+    <?= displayErrors($errors); ?>
+<?php endif; ?>
+
+<div class="container">
     <button type="button" id="add-task" class="btn btn-primary" data-toggle="modal" data-target="#add-task-modal">
         <i class="fa fa-plus"></i> Add new task
     </button>
     
     <?php if (count($tasks) > 0) : ?>
         <div class="col-sm-8 offset-2">
-            <?php if ($error !== null) : ?>
-                <div class="alert alert-danger alert-dismissable fade show text-center">
-                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    <strong><?= $error ?></strong>
-                </div>
-            <?php endif; ?>
             <div class="table-responsive">          
                 <table class="table table-striped" class="thead-dark">
                     <thead class="thead-dark">
@@ -31,11 +33,11 @@
                                 <td><?= $task->id ?></td>
                                 <td><?= $task->description ?></td>
                                 <?php if ($task->completed) : ?>
-                                    <td>
+                                    <td id="1">
                                         <i class="fa fa-check"></i>
                                     </td>
                                 <?php else : ?>
-                                    <td>
+                                    <td id="0">
                                         <i class="fa fa-spinner fa-pulse"></i>
                                     </td>
                                 <?php endif; ?>
@@ -53,6 +55,9 @@
                 </table>
             </div>
         </div>
+        <?php if (isset($pagination)) : ?>
+            <?= $pagination->links('/tasks'); ?>
+        <?php endif; ?>
     <?php endif; ?>
 </div>
 
