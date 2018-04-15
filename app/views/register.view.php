307901a94603eaa91c4f3beb12edadeed7788f9c
Florian B <floryn93@gmail.com>
2018-04-15 12:04:32 +0300

    Add the form into a card and display errors alert.

--- app/views/register.view.php
+++ app/views/register.view.php
@@ -1,33 +1,44 @@
 <?php require 'partials/head.php'; ?>
 
+<?php if ($errors) : ?>
+    <?= displayErrors($errors); ?>
+<?php endif; ?>
+
 <div class="container">
-    <h1 class="display-1">Sign Up</h1>
     <div class="row">
         <div class="col-sm-6 offset-3">
-            <form method="POST" action="\register">
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
-                        <input type="text" class="form-control" name="username" id="username" placeholder="Username" required autofocus>
-                    </div>
+            <div class="card">
+                <div class="card-header bg-primary">
+                <h1 class="display-1 text-light">Register</h1>
                 </div>
-                
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-lock fa-fw"></i></span>
-                    <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
-                    </div>
-                </div>
+                <div class="card-body">
+                    <form method="POST" action="\register">
+                        <div class="form-group">
+                            <label for="username">
+                                <b>Username:</b>
+                            </label>
+                            <input type="text" class="form-control" name="user[username]" id="username" placeholder="Your username" required autofocus>
 
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-lock fa-fw"></i></span>
-                        <input type="password" class="form-control" name="passwordConfirmation" id="passwordConfirmation" placeholder="Password Confirmation" required>
-                    </div>
-                </div>
+                        </div>
 
-                <button type="submit" class="btn btn-success btn-block">Submit</button>
-            </form>
+                        <div class="form-group">
+                            <label for="password">
+                                <b>Password:</b>
+                            </label>
+                            <input type="password" class="form-control" name="user[password]" id="password" placeholder="Your password" required>
+                        </div>
+
+                        <div class="form-group">
+                        <label for="password_confirmation">
+                                <b>Password confirmation:</b>
+                            </label>
+                            <input type="password" class="form-control" name="user[password_confirmation]" id="password_confirmation" placeholder="Confirm your password" required>
+                        </div>
+
+                        <button type="submit" class="btn btn-success btn-block">Register</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
