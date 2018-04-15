b74b3ca10898bd29ff9d09571161554ff800223e
Florian B <floryn93@gmail.com>
2018-04-15 12:00:59 +0300

    Add Session and BaseModel.

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,12 +1,15 @@
 <?php 
 
-session_start();
-
 use App\Core\App;
-use App\Core\Database\{QueryBuilder, Connection};
+use App\Core\Session;
+use App\Core\Database\{Connection, BaseModel, QueryBuilder};
 
-App::bind('config', require 'config.php');
+define('PROJECT_PATH', dirname($_SERVER['DOCUMENT_ROOT']));
 
+App::bind('config', require_once(PROJECT_PATH . '/config.php'));
+App::bind('session', new Session());
 App::bind('database', new QueryBuilder(
     Connection::make(App::get('config')['database'])
 ));
+
+BaseModel::setDatabase(Connection::make(App::get('config')['database']));
