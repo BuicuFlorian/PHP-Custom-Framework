9dfb5f0564cd9c16c75d0a27233b0edee5e1b9a2
Florian B <floryn93@gmail.com>
2018-04-15 11:57:44 +0300

    Format the source code.

--- core/Request.php
+++ core/Request.php
@@ -2,27 +2,28 @@
 
 namespace App\Core;
 
-class Request 
+class Request
 {
     /**
      * Get the uri from the request.
-     * 
+     *
      * @return string $uri
      */
     public static function uri()
     {
         return trim(
-            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
+            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
+            '/'
         );
     }
 
     /**
      * Get the method from the request.
-     * 
+     *
      * @return string $method
      */
     public static function method()
     {
         return $_SERVER['REQUEST_METHOD'];
     }
-}  
+}
