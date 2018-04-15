f319aa11d96409bae2ab416a00be7673b6d2b2ce
Florian B <floryn93@gmail.com>
2018-04-15 11:46:57 +0300

    Add app and mail arrays.

--- config.php
+++ config.php
@@ -1,13 +1,25 @@
 <?php
 
 return [
+    'app' => [
+        'name' => 'PHP Framework',
+        'url' => 'http://localhost:8000'
+    ],
+
     'database' => [
-        'name' => '',
+        'name' => 'php_framework',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
+    ],
+
+    'mail' => [
+        'username' => '',
+        'password' => '',
+        'from' => '',
+        'reply_to' => ''
     ]
 ];
